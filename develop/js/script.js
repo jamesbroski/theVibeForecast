fa368c27a18b0d36ac92279df72ce6e3fc7ae89c
Patrick Brown <breepatbree@gmail.com>
2021-08-12 12:05:20 -0600

    Youtube array working

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,4 +1,5 @@
 //city search click event to record the weather parameter in your city of choice
+var youtubeVideos = [];
 
 $("#citySearchBtn").on("click", function () {
   event.preventDefault();
@@ -11,7 +12,7 @@ $("#citySearchBtn").on("click", function () {
     url: searchURL,
     method: "GET",
   }).then(function (data) {
-    console.log(data.list[0].weather[0].main);
+    console.log(data);
     var localVibe = data.list[0].weather[0].main;
     //sets the link for youtube api pull based on if the weather is encouraging relaxing or encouraging excitement
     if (localVibe == "Clouds" || localVibe == "Clear" || localVibe == "Snow") {
@@ -27,6 +28,12 @@ $("#citySearchBtn").on("click", function () {
         "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=rainy_day_music&type=video&videoLicense=creativeCommon&videoEmbeddable=true&videoSyndicated=true&key=AIzaSyCdlZo2lbxsoVE0a4K6d8pB2Z66ypZR_40";
     }
     //return youtubeSRC external ajax call, which will generate api data to randomly pick you a video based on weather parameters
+    $("#weatherCity").text(data.city.name)
+    $("#weatherCondition").text(localVibe)
+    $("#weatherTemp").text(data.list[0].main.temp+"°"+"F")
+    $("#weatherDate").text(data.list[0].dt_txt)
+    $("#weatherFeels").text("The temperature vibe feels like "+data.list[0].main.feels_like+" °"+"F")
+
 
     $.ajax({
       url: youtubeSRC,
@@ -39,7 +46,18 @@ $("#citySearchBtn").on("click", function () {
       console.log(youtubeTag);
       $("#youtubePlayer").attr("src","https://www.youtube.com/embed/"+youtubeTag);
 
+      if(youtubeVideos.indexOf(youtubeTag)<0) {
+        youtubeVideos.push(youtubeTag)
+      }
+
+
+      localStorage.setItem("video",JSON.stringify(youtubeVideos))
+      return youtubeTag
     })
+
+
+
+
 
   });
 });
